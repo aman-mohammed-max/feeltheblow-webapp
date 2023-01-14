5d0ac8564eec15bdd082a86c37c32ac3c13a497c
aman-mohammed-max <74004117+aman-mohammed-max@users.noreply.github.com>
2023-01-14 15:47:14 +0530

    fixed the sliders

--- src/Global/Components/Sliders/Sliders.js
+++ src/Global/Components/Sliders/Sliders.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import './Sliders.css'
-import PropTypes from 'prop-types';
+import React, { useRef , useEffect } from "react";
+import "./Sliders.css";
+import PropTypes from "prop-types";
 
-function Sliders(props) {  
+export default function Sliders(props) {
+  const sliderref = useRef(null);
+  useEffect(() => {
+    if(sliderref.current){
+      sliderref.current.style.setProperty("--value", sliderref.current.value);
+      sliderref.current.style.setProperty("--min", sliderref.current.min === "" ? "0" : sliderref.current.min);
+      sliderref.current.style.setProperty("--max", sliderref.current.max === "" ? "100" : sliderref.current.max);
+      sliderref.current.addEventListener("input", () => sliderref.current.style.setProperty("--value", sliderref.current.value));
+    }
+  }, []);
   return (
     <div>
-        <input type="range" min={props.min} max={props.max} step={props.step} onChange={props.onChange} value={props.value} className="styled-slider slider-progress" style={props.style} />
+      <input
+        ref={sliderref}
+        type="range"
+        min={props.min}
+        max={props.max}
+        step={props.step}
+        onChange={props.onChange}
+        value={props.value}
+        className="styled-slider slider-progress"
+        style={props.style}
+      />
     </div>
-  )
+  );
 }
 
 Sliders.propTypes = {
-    min: PropTypes.number,
-    max: PropTypes.number,
-    onChange: PropTypes.func,
-    value: PropTypes.number,
-    step: PropTypes.number,
-    style: PropTypes.object
-}
+  min: PropTypes.number,
+  max: PropTypes.number,
+  onChange: PropTypes.func,
+  value: PropTypes.number,
+  step: PropTypes.number,
+  style: PropTypes.object,
+};
 
 Sliders.defaultProps = {
-    min: 0,
-    max: 100,
-}
-
-export default Sliders
+  min: 0,
+  max: 100,
+};
